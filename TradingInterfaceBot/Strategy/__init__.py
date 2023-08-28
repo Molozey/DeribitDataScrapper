5882c6fa4958d2d1e6514d9399c2cc5f1d39b3df
Molozey <81177999+Molozey@users.noreply.github.com>
2023-08-28 04:30:27 +0300

    Delete TradingInterfaceBot directory
    clear all main

--- TradingInterfaceBot/Strategy/__init__.py
+++
@@ -1,4 +0,0 @@
-from .BasicStrategy import BaseStrategy
-from .EmptyStrategy import EmptyStrategy
-from .AbstractStrategy import AbstractStrategy
-from .TickerNode import TickerNode
--- TradingInterfaceBot/Utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .AvailableCurrencies import Currency
-from .MSG_LIST import *
-from .mysqlRecording.cleanUpRequestsLimited import *
-from .mysqlRecording.postDataTemplateLimited import *
-from .AvailableInstrumentType import *
